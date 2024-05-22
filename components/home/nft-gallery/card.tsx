15dbaa0be8c22aabaf1a938d40b9ca6ee6c26953
Cuong Dao <christiandao173@gmail.com>
2024-05-23 00:33:50 +0700

    fix nft gallery

--- components/home/nft-gallery/card.tsx
+++ components/home/nft-gallery/card.tsx
@@ -1,68 +1,58 @@
+import { cn } from '@/lib/utils'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import React from 'react'
 
 export type Props = {
-  front: string
-  back: string
-
-  frontMobile: string
-  backMobile: string
-
   icon: string
-  title: string
-  body: string
+  bgMobile: string
+  bgDesktop: string
+  color: string
+
+  description: string
   motionProps: any
 }
 
-export default function Card({ ...props }: Props) {
+const Card: React.FC<Props> = ({ icon, bgDesktop, bgMobile, color, description, motionProps }) => {
   return (
-    <motion.div
-      {...props.motionProps}
-      className="flip-card group relative w-full max-w-[400px] lg:max-w-auto mx-auto lg:mx-0 "
-    >
-      <div className="flip-card-inner ">
-        {/* Front */}
-        <div
-          className="absolute inset-0"
-          style={{
-            WebkitBackfaceVisibility: 'hidden',
-            backfaceVisibility: 'hidden',
-          }}
-        >
-          <Image src={props.front} width={235} height={417} alt="" className="hidden object-cover lg:inline-block" />
-          <Image src={props.frontMobile} width={400} height={200} alt="" className="object-cover lg:hidden" />
+    <motion.div {...motionProps} className=" group relative w-full max-w-[400px] lg:max-w-auto mx-auto lg:mx-0 ">
+      <div className="group relative overflow-hidden">
+        <div>
+          <Image src={bgDesktop} width={235} height={417} alt="" className="hidden object-cover lg:inline-block" />
+          <Image src={bgMobile} width={400} height={200} alt="" className="object-cover lg:hidden" />
         </div>
 
-        {/* Back */}
         <div
-          className="rotate absolute inset-0 flex flex-col items-center justify-between bg-full min-h-[210px] lg:min-h-[417px]"
-          style={{
-            WebkitBackfaceVisibility: 'hidden',
-            backfaceVisibility: 'hidden',
-            transform: 'rotateY(180deg)',
-          }}
+          className={cn(
+            'absolute top-1/2 left-1/2',
+            '-translate-x-1/2 -translate-y-1/2',
+            'max-lg:group-hover:left-[20%]',
+            'lg:group-hover:top-[40%]',
+            'transition-all duration-200 z-10',
+            'w-[82px] h-[82px]',
+          )}
         >
-          <div className="relative w-full">
-            <Image src={props.back} width={235} height={417} alt="" className="hidden object-cover lg:inline-block" />
-            <Image src={props.backMobile} width={400} height={200} alt="" className="object-cover lg:hidden" />
+          <Image src={icon} width={82} height={82} alt="" />
+        </div>
 
-            <div className="absolute bottom-1/2 translate-y-[50%] max-w-[70%] lg:max-w-full lg:translate-y-[80%] right-0 lg:right-[unset] lg:left-0 px-7">
-              <p className="relative z-[2] text-[3.5vw] sm:text-sm  font-Roboto text-left lg:text-center leading-normal tracking-[-0.2px] text-secondary">
-                {props.body}
-              </p>
-            </div>
-          </div>
+        <div
+          style={{
+            backgroundColor: color,
+          }}
+          className={cn(
+            'absolute max-lg:right-0 bottom-0',
+            'transition-all duration-200',
+            'max-lg:translate-x-full max-lg:group-hover:translate-x-0',
+            'lg:translate-y-full lg:group-hover:translate-y-[10%]',
+            'flex items-center justify-center',
+            'pl-14 pr-10 w-[80%]  h-full lg:px-7  lg:w-full  lg:h-[65%]',
+            'rounded-tl-[20px] rounded-bl-[20px] lg:rounded-tr-[20px]',
+          )}
+        >
+          <div className="text-secondary text-sm font-Roboto text-left lg:text-center">{description}</div>
         </div>
       </div>
-
-      <Image
-        src={props.front}
-        width={235}
-        height={417}
-        alt=""
-        className="hidden object-cover lg:inline-block opacity-0"
-      />
-      <Image src={props.frontMobile} width={400} height={200} alt="" className="object-cover lg:hidden opacity-0" />
     </motion.div>
   )
 }
+export default Card
--- constants/gallery-card.constant.ts
+++ constants/gallery-card.constant.ts
@@ -3,74 +3,67 @@ import { flyInAnimation } from '@/constants/animation.constant'
 
 export const cards: CardProps[] = [
   {
-    front: '/images/home/nft-gallery/cards/red/front.png',
-    back: '/images/home/nft-gallery/cards/red/back.png',
-    frontMobile: '/images/home/nft-gallery/cards/red/front-mobile.png',
-    backMobile: '/images/home/nft-gallery/cards/red/back-mobile.png',
+    icon: '/images/home/nft-gallery/cards/fire/fire-icon.png',
+    bgMobile: '/images/home/nft-gallery/cards/fire/fire-bg-mobile.png',
+    bgDesktop: '/images/home/nft-gallery/cards/fire/fire-bg.png',
+    color: '#56090B',
 
-    icon: '/images/five-elements/fire.svg',
-    title: '/Fire',
-    body: '“Uncover the unparalleled allure of Flame NFTs, where each piece ignites with its own distinct essence.”',
+    description:
+      '“Uncover the unparalleled allure of Flame NFTs, where each piece ignites with its own distinct essence.”',
     motionProps: {
       transition: { duration: 0.5, delay: 1 },
       ...flyInAnimation,
     },
   },
   {
-    front: '/images/home/nft-gallery/cards/brown/front.png',
-    back: '/images/home/nft-gallery/cards/brown/back.png',
-    frontMobile: '/images/home/nft-gallery/cards/brown/front-mobile.png',
-    backMobile: '/images/home/nft-gallery/cards/brown/back-mobile.png',
+    icon: '/images/home/nft-gallery/cards/earth/earth-icon.png',
+    bgMobile: '/images/home/nft-gallery/cards/earth/earth-bg-mobile.png',
+    bgDesktop: '/images/home/nft-gallery/cards/earth/earth-bg.png',
+    color: '#6B3C02',
 
-    icon: '/images/five-elements/earth.svg',
-    title: '/Earth',
-    body: '“Discover the unmatched charm of Earth NFTs, where each piece reflects the essence of the land”',
+    description: '“Discover the unmatched charm of Earth NFTs, where each piece reflects the essence of the land”',
     motionProps: {
       transition: { duration: 0.5, delay: 1.3 },
       ...flyInAnimation,
     },
   },
   {
-    front: '/images/home/nft-gallery/cards/yellow/front.png',
-    back: '/images/home/nft-gallery/cards/yellow/back.png',
-    frontMobile: '/images/home/nft-gallery/cards/yellow/front-mobile.png',
-    backMobile: '/images/home/nft-gallery/cards/yellow/back-mobile.png',
+    icon: '/images/home/nft-gallery/cards/mental/mental-icon.png',
+    bgMobile: '/images/home/nft-gallery/cards/mental/mental-bg-mobile.png',
+    bgDesktop: '/images/home/nft-gallery/cards/mental/mental-bg.png',
+    color: '#A17015',
 
-    icon: '/images/five-elements/mental.svg',
-    title: '/Metal',
-    body: '“Explore the captivating allure of Metal NFTs, each piece embodying the strength and brilliance of precious metals.” ',
+    description:
+      '“Explore the captivating allure of Metal NFTs, each piece embodying the strength and brilliance of precious metals.” ',
     motionProps: {
       transition: { duration: 0.5, delay: 1.6 },
       ...flyInAnimation,
     },
   },
   {
-    front: '/images/home/nft-gallery/cards/green/front.png',
-    back: '/images/home/nft-gallery/cards/green/back.png',
-    frontMobile: '/images/home/nft-gallery/cards/green/front-mobile.png',
-    backMobile: '/images/home/nft-gallery/cards/green/back-mobile.png',
+    icon: '/images/home/nft-gallery/cards/wood/wood-icon.png',
+    bgMobile: '/images/home/nft-gallery/cards/wood/wood-bg-mobile.png',
+    bgDesktop: '/images/home/nft-gallery/cards/wood/wood-bg.png',
+    color: '#023129',
 
-    icon: '/images/five-elements/wood.svg',
-    title: '/Wood',
-    body: '“Dive into the rustic charm of Wood NFTs, where each artwork reflects the natural beauty and resilience of timber.”',
+    description:
+      '“Dive into the rustic charm of Wood NFTs, where each artwork reflects the natural beauty and resilience of timber.”',
     motionProps: {
       transition: { duration: 0.5, delay: 1.9 },
       ...flyInAnimation,
     },
   },
   {
-    front: '/images/home/nft-gallery/cards/blue/front.png',
-    back: '/images/home/nft-gallery/cards/blue/back.png',
-    frontMobile: '/images/home/nft-gallery/cards/blue/front-mobile.png',
-    backMobile: '/images/home/nft-gallery/cards/blue/back-mobile.png',
+    icon: '/images/home/nft-gallery/cards/water/water-icon.png',
+    bgMobile: '/images/home/nft-gallery/cards/water/water-bg-mobile.png',
+    bgDesktop: '/images/home/nft-gallery/cards/water/water-bg.png',
+    color: '#11184C',
 
-    icon: '/images/five-elements/water.svg',
-    title: '/Water',
-    body: '“Delve into the fluid elegance of Water NFTs, each piece capturing the essence of fluidity and serenity."',
+    description:
+      '“Delve into the fluid elegance of Water NFTs, each piece capturing the essence of fluidity and serenity."',
     motionProps: {
       transition: { duration: 0.5, delay: 2.1 },
       ...flyInAnimation,
     },
   },
 ]
-
