f04700627f9c9a974bddd6194de5103328a5a7bc
Cuong Dao <christiandao173@gmail.com>
2024-05-23 01:04:23 +0700

    update mobile side bar

--- components/header/mobile-header.tsx
+++ components/header/mobile-header.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import MobileSidebar from './mobile-sidebar'
+import { cn } from '@/lib/utils'
 
 export default function MobileHeader() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
   const path = usePathname()
   const isHomePage = path === '/'
   const [show, setShow] = useState(true)
@@ -38,28 +38,18 @@ export default function MobileHeader() {
   return (
     <>
       <header
-        className={`${isHomePage ? (show ? 'scale-y-100' : 'scale-y-0') : ''} fixed inset-x-0 top-0 z-[100] origin-top transition-all lg:hidden`}
+        className={cn(
+          isHomePage ? (show ? 'scale-y-100' : 'scale-y-0') : '',
+          `fixed inset-x-0 top-0 z-[100] origin-top transition-all lg:hidden`,
+        )}
       >
-        <div className='flex h-[72px] w-full items-center justify-between bg-[#FAF5F0] px-4'>
-          <Link href='/' className='flex items-center gap-[7.74px]'>
-            <Image src={logo} alt='' className='h-9 w-[123.14217px]' />
+        <div className="flex h-[72px] w-full items-center justify-between bg-secondary px-4">
+          <Link href="/" className="flex items-center gap-[7.74px]">
+            <Image src={logo} alt="" className="h-9 w-[123.14217px]" />
           </Link>
-          <button
-            className='inline-block size-10 p-2'
-            onClick={() => {
-              setSidebarOpen((prev) => !prev)
-            }}
-          >
-            <Image src={menuIcon} alt='' className='size-6' />
-          </button>
+          <MobileSidebar />
         </div>
       </header>
-      <MobileSidebar
-        open={sidebarOpen}
-        onClose={() => {
-          setSidebarOpen(false)
-        }}
-      />
     </>
   )
 }
--- components/header/mobile-sidebar.tsx
+++ components/header/mobile-sidebar.tsx
@@ -1,66 +1,59 @@
 'use client'
 
 import { headerItems } from '@/constants/header.constant'
-import closeModalButton from '@/icons/profile/modal/close-button.svg'
-import { Language } from '@/types/language'
-import Image from 'next/image'
+import { cn } from '@/lib/utils'
+import { Menu, X } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { Fragment, useState } from 'react'
 import WrapperHero from '../home/WrapperHero'
-import ModalContainer from '../ui/modal-container'
+import { Drawer, DrawerClose, DrawerContent, DrawerFooter, DrawerTrigger } from '../ui/drawer'
 import ConnectWalletButton from './connect-wallet-button'
 
-type Props = {
-  open: boolean
-  onClose: () => void
-}
-
-export default function MobileSidebar({ open, onClose }: Props) {
+const MobileSidebar: React.FC = () => {
   const path = usePathname()
-  const isHomePage = path === '/'
-
-  const [language, setLanguage] = useState<Language>('en')
 
   return (
-    <ModalContainer open={open} handleClose={onClose}>
-      <WrapperHero src="/images/home/mobile-background.png">
-        <div className="w-screen">
-          <div className="h-screen w-full flex flex-col justify-between relative  px-[28px] py-[48px]">
-            <button
-              onClick={onClose}
-              className="absolute right-6 top-6 z-10 outline-none md:right-10 md:top-10 hover:bg-[rgba(212,199,156,0.30)] hover:rounded-md"
-            >
-              <Image src={closeModalButton} alt="" width={38} height={38} className=" fill-black1 stroke-black1" />
+    <Drawer direction="right">
+      <DrawerTrigger asChild>
+        <button>
+          <Menu />
+        </button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <WrapperHero src="/images/home/mobile-background.png" className="py-12 px-7">
+          <DrawerClose>
+            <button className="absolute right-6 top-6 z-10 outline-none p-2 hover:bg-bgAlt4d hover:rounded-md">
+              <X className="fill-black1 w-7 h-7" />
             </button>
-            <div className="flex w-[305px] flex-col gap-8 bg-full text-text-black">
-              <div className="flex w-[207.62px] flex-col gap-6">
-                <div className="flex h-full w-full flex-col gap-6">
-                  {headerItems.map((item, index) => (
-                    <Fragment key={index}>
-                      <Link
-                        href={item.href}
-                        className={`${(item.href === '/' && isHomePage) || (path.includes(item.href) && !isHomePage && item.href !== '/') ? ' text-red-light' : 'border-transparent'} 
-                        flex h-full w-full gap-2 px-6 text-[22px] font-semibold`}
-                      >
-                        <span>{item.label}</span>
-                      </Link>
-                    </Fragment>
-                  ))}
-                  <Link
-                    href="/profile"
-                    className={`${path.includes('/profile') ? ' text-red-light' : 'border-transparent'} flex h-full w-full gap-2 px-6 text-[22px] font-semibold`}
-                  >
-                    <span>My Profile</span>
-                  </Link>
-                </div>
-              </div>
+          </DrawerClose>
+          <div className="flex flex-col">
+            <div className="flex w-full flex-col gap-6">
+              {[
+                ...headerItems,
+                {
+                  href: '/profile',
+                  label: 'My Profile',
+                },
+              ].map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.href}
+                  className={cn(
+                    item.href === path ? 'text-red-light' : 'text-text-black',
+                    `flex w-full gap-2 px-6 text-[22px] font-semibold`,
+                  )}
+                >
+                  <DrawerClose>{item.label}</DrawerClose>
+                </Link>
+              ))}
             </div>
-            <ConnectWalletButton mode="solid" />
           </div>
-        </div>
-      </WrapperHero>
-    </ModalContainer>
+          <DrawerFooter className="p-0">
+            <ConnectWalletButton mode="solid" />
+          </DrawerFooter>
+        </WrapperHero>
+      </DrawerContent>
+    </Drawer>
   )
 }
-
+export default MobileSidebar
