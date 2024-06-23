a91b131a13592a5da0eb470895a20f2dfc37f267
Cuong Dao <christiandao173@gmail.com>
2024-06-23 18:07:02 +0700

    update hard  cord Testnet

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId } from '@/hooks/WalletProvider/connectors/base'
 
-export const CHAIN_ID = ChainId.Mainnet
+export const CHAIN_ID = ChainId.Testnet
 export const BEARER_UNISAT = process.env.NEXT_PUBLIC_BEARER_UNISAT
 
 export const walletAuthenticated = 'wallet-authenticated'
